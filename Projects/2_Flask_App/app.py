7aa39d5a18e2f500435ad64a97e4eb897d5cb90d
PhucBe <phucvu011203@gmail.com>
2026-04-26 23:10:53 +0700

    docker network & docker compose

--- Projects/2_Flask_App/app.py
+++ Projects/2_Flask_App/app.py
@@ -1,3 +1,18 @@
 from flask import Flask
 
-app 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask App running in Docker!"
+
+@app.route("/about")
+def about():
+    return "This is a simple Flask application running inside a Docker container."
+
+@app.route("/contact")
+def contact():
+    return "Contact us at: abc"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ Projects/3_Docker_App_Network/MySQL/init.sql
@@ -0,0 +1,7 @@
+USE DATABASE demodb;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    city VARCHAR(100) NOT NULL,
+    temperature FLOAT NOT NULL
+);
